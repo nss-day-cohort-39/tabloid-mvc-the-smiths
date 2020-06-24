568c63f42f8d635641999afcb9ea9cbe3995b1b1
Daniel Hero <danielhero@outlook.com>
2020-06-24 15:14:40 -0500

    added model/repo/controller for comments

---
+++ TabloidMVC/Controllers/CommentController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly CommentRepository _commentRepository;
+
+        public CommentController(IConfiguration config)
+        {
+            _commentRepository = new CommentRepository(config);
+        }
+
+        // GET: CommentController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CommentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CommentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CommentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentController/Edit/5
+        public IActionResult EditComment(int id)
+        {
+            var comment = _commentRepository.GetCommentById(id);
+
+            if (comment == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(comment);
+        }
+
+        // POST: CommentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, Comment comment)
+        {
+            try
+            {
+                _commentRepository.UpdateComment(comment);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View(comment);
+            }
+        }
+
+        // GET: CommentController/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var comment =_commentRepository.GetCommentById(id);
+
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return View(comment);
+        }
+
+        // POST: CommentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id, Comment comment)
+        {
+            try
+            {
+                _commentRepository.DeleteComment(comment);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View(comment);
+            }
+        }
+    }
+}
---
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class CommentRepository : BaseRepository
+    {
+        public CommentRepository(IConfiguration config) : base(config) { }
+
+        public Comment GetCommentById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                       SELECT c.Id, c.PostId, c.UserProfileId, 
+                              c.Subject, c.Content, c.CreateDateTime,
+                              p.Title, p.Content,
+                              p.ImageLocation AS HeaderImage,
+                              p.CreateDateTime, p.PublishDateTime, p.IsApproved,
+                              p.CategoryId, p.UserProfileId,
+                              u.FirstName, u.LastName, u.DisplayName, 
+                              u.Email, u.CreateDateTime, u.ImageLocation AS AvatarImage,
+                              u.UserTypeId, 
+                         FROM Comment c
+                              LEFT JOIN Post p ON c.PostId = p.id
+                              LEFT JOIN UserProfile u ON c.UserProfileId = u.id
+                              AND c.id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+                    var reader = cmd.ExecuteReader();
+
+                    Comment comment = null;
+
+                    if (reader.Read())
+                    {
+                        comment = new Comment();
+                        comment.Id = reader.GetInt32(reader.GetOrdinal("Id"));
+                        comment.PostId = reader.GetInt32(reader.GetOrdinal("PostId"));
+                        comment.UserProfileId = reader.GetInt32(reader.GetOrdinal("UserProfileId"));
+                        comment.Subject = reader.GetString(reader.GetOrdinal("Subject"));
+                        comment.Content = reader.GetString(reader.GetOrdinal("Content"));
+                        comment.CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"));
+                    }
+
+                    reader.Close();
+
+                    return comment;
+                }
+            }
+        }
+
+        public Comment GetUserCommentById(int id, int userProfileId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                       SELECT c.Id, c.PostId, c.UserProfileId, 
+                              c.Subject, c.Content, c.CreateDateTime,
+                              p.Title, p.Content,
+                              p.ImageLocation AS HeaderImage,
+                              p.CreateDateTime, p.PublishDateTime, p.IsApproved,
+                              p.CategoryId, p.UserProfileId,
+                              u.FirstName, u.LastName, u.DisplayName, 
+                              u.Email, u.CreateDateTime, u.ImageLocation AS AvatarImage,
+                              u.UserTypeId, 
+                         FROM Comment c
+                              LEFT JOIN Post p ON c.PostId = p.id
+                              LEFT JOIN UserProfile u ON c.UserProfileId = u.id
+                        WHERE c.id = @id AND c.UserProfileId = @userProfileId";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.Parameters.AddWithValue("@userProfileId", userProfileId);
+                    var reader = cmd.ExecuteReader();
+
+                    Comment comment = null;
+
+                    if (reader.Read())
+                    {
+                        comment = new Comment();
+                        comment.Id = reader.GetInt32(reader.GetOrdinal("Id"));
+                        comment.PostId = reader.GetInt32(reader.GetOrdinal("PostId"));
+                        comment.UserProfileId = reader.GetInt32(reader.GetOrdinal("UserProfileId"));
+                        comment.Subject = reader.GetString(reader.GetOrdinal("Subject"));
+                        comment.Content = reader.GetString(reader.GetOrdinal("Content"));
+                        comment.CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"));
+                    }
+
+                    reader.Close();
+
+                    return comment;
+                }
+            }
+        }
+
+        public void DeleteComment(Comment comment)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                DELETE FROM Comment
+                                WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", comment.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void UpdateComment(Comment comment)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                               UPDATE Comment
+                               SET
+                                    Subject = @subject,
+                                    Content = @content
+                               WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@subject", comment.Subject);
+                    cmd.Parameters.AddWithValue("@content", comment.Content);
+                    cmd.Parameters.AddWithValue("@id", comment.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
