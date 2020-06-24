0c92158d9f096f653737d076b8b67d2c245139ff
blake slochowsky <blakeslochowsky.com>
2020-06-24 09:58:46 -0500

    viewing categories

---
+++ TabloidMVC/Controllers/CategoryController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    [Authorize]
+    public class CategoryController : Controller
+    {
+
+        private CategoryRepository _categoryRepository;
+            public CategoryController(IConfiguration config)
+            {
+            _categoryRepository = new CategoryRepository(config);
+            }
+
+
+        // GET: RepositoryController
+        public IActionResult Index()
+        {
+            List<Category> categories = _categoryRepository.GetAll();
+            
+            return View(categories);
+        }
+
+        // GET: RepositoryController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RepositoryController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RepositoryController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RepositoryController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RepositoryController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RepositoryController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RepositoryController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- TabloidMVC/Repositories/CategoryRepository.cs
+++ TabloidMVC/Repositories/CategoryRepository.cs
@@ -1,20 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection.PortableExecutable;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using TabloidMVC.Models;
+using TabloidMVC.Utils;
 
 namespace TabloidMVC.Repositories
 {
     public class CategoryRepository : BaseRepository
     {
+
         public CategoryRepository(IConfiguration config) : base(config) { }
         public List<Category> GetAll()
         {
+
             using (var conn = Connection)
             {
                 conn.Open();
                 using (var cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = "SELECT id, name FROM Category";
+                    cmd.CommandText = @"SELECT Id, Name 
+                                        FROM Category
+                                        ORDER BY Name ASC";
                     var reader = cmd.ExecuteReader();
 
                     var categories = new List<Category>();
@@ -24,15 +33,46 @@ namespace TabloidMVC.Repositories
                         categories.Add(new Category()
                         {
                             Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                            Name = reader.GetString(reader.GetOrdinal("name")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
                         });
                     }
 
                     reader.Close();
 
                     return categories;
+
+                }
+            }
+        }
+        public Category GetCategoryById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT id, name 
+                                        FROM Category AND Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+                    var reader = cmd.ExecuteReader();
+
+                    Category category = null;
+
+                    if (reader.Read())
+                    {
+                        category = new Category();
+                        category.Id = reader.GetInt32(reader.GetOrdinal("Id"));
+                        category.Name = reader.GetString(reader.GetOrdinal("Name"));
+                    }
+
+                    reader.Close();
+
+                    return category;
                 }
             }
         }
     }
 }
+        
+    
